dfff945f89df85a70c6c083af1bd357196ae1299
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-07-25 22:55:38 -0600

    Js, CSS, readme
    Js- added running clock
    CSS- final draft on styling
    readme- added banner, screenshot and github link
    
    Need to: Js- function to "tell time" add/delete (past, present, future) & save/recall text for hourblocks
      readme- update screenshot, deploy link & final text content/layout

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,3 +1,11 @@
 // get current date
-var currentDay = moment().format("dddd, MMMM Do");
+var currentDay = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentDay);
+
+// get running clock
+let clock = document.querySelector("#clock");
+
+setInterval(function () {
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
