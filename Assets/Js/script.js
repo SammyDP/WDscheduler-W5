7a33b7c93c3ed3c6806edb9584670eed0712ad63
Sammy <107449948+SammyDP@users.noreply.github.com>
2022-07-26 22:26:05 -0600

    Js, readme
    Js- worked on clock checker but needs a for loop and dont have time
    readme- finished links, ready to submit

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -14,47 +14,44 @@ setInterval(function () {
 
 // clock checker
 // -------------
-var hr9 = document.querySelector("#hr9");
-var hr10 = document.querySelector("#hr10");
-var hr11 = document.querySelector("#hr11");
-var hr12 = document.querySelector("#hr12");
-var hr1 = document.querySelector("#hr1");
-var hr2 = document.querySelector("#hr2");
-var hr3 = document.querySelector("#hr3");
-var hr4 = document.querySelector("#hr4");
-var hr5 = document.querySelector("#hr5");
 
-// if/else-if/else to check time and change "past, present, future"
-function clockCheck() {
-  var timeBlk = document.getAttribute("data-value");
-  var currTime = moment().hour();
+// for loop, change all timeblock check each block, target class for array
+var hourAry = document.querySelector(".hour");
+var currTime = moment().hour();
 
-  if (currTime < timeBlk) {
-    timeBlk.addClass(".past");
-    timeBlk.removeClass(".present");
-    timeBlk.removeClass(".future");
-  } else if (currTime === timeBlk) {
-    timeBlk.addClass(".past");
-    timeBlk.removeClass(".present");
-    timeBlk.removeClass(".future");
-  } else {
-    timeBlk.addClass(".past");
-    timeBlk.removeClass(".presnt");
-    timeBlk.removeClass(".future");
-  }
-  clockCheck();
-}
+$(hourAry).each(function name(params) {});
+
+// function clockCheck() {
+//   var timeBlk = document.querySelector("#hr9");
+//   var currTime = moment().hour();
+//   console.log(currTime);
+//   console.log(timeBlk.dataset.value);
+//   if (currTime < timeBlk) {
+//     timeBlk.addClass(".past");
+//     timeBlk.removeClass(".present");
+//     timeBlk.removeClass(".future");
+//   } else if (currTime === timeBlk) {
+//     timeBlk.addClass(".past");
+//     timeBlk.removeClass(".present");
+//     timeBlk.removeClass(".future");
+//   } else {
+//     timeBlk.addClass(".past");
+//     timeBlk.removeClass(".presnt");
+//     timeBlk.removeClass(".future");
+//   }
+// }
+// clockCheck();
 
 // save button
 // -----------
-var saveBtn = document.querySelector(".saveBtn");
-var work = localStorage.getItem(time, text) || 0;
+// var saveBtn = document.querySelector(".saveBtn");
+// var work = localStorage.getItem(time, text) || 0;
 
-function saveWork() {
-  saveBtn.addEventListener("click", () => {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, text);
-    // data recall (localStorage.getItem(time, text))
-  });
-}
+// function saveWork() {
+//   saveBtn.addEventListener("click", () => {
+//     var text = $(this).siblings(".description").val();
+//     var time = $(this).parent().attr("id");
+//     localStorage.setItem(time, text);
+//     // data recall (localStorage.getItem(time, text))
+//   });
+// }
